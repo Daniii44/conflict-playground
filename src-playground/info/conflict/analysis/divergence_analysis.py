6d830db73c8e19b950ebd0053a5f8b93be99ae80
Daniel Gruber <daniel.gruber@altofing.de>
2026-05-20 20:49:56 +0200

    feat(info-conflict): entangled_commit_count for octopus merges

--- src-playground/info/conflict/analysis/divergence_analysis.py
+++ src-playground/info/conflict/analysis/divergence_analysis.py
@@ -6,7 +6,7 @@ from info.conflict.analysis.common import Analysis, AnalysisInput, InfoConflict
 class InfoConflictDivergence(InfoConflict):
     parent_count: int
     merge_base_count: int
-    entangled_commit_count: int | None  # None if parent_count != 2
+    entangled_commit_count: int
 
 
 class DivergenceAnalysis(Analysis):
@@ -20,10 +20,10 @@ class DivergenceAnalysis(Analysis):
         merge_base_cmd = ["git", f"--git-dir={analysisInput.git_dir}", "merge-base", "-a"] + parents
         merge_bases = subprocess.check_output(merge_base_cmd, text=True).strip().split()
 
-        entangled_commit_count = None
-        if len(parents) == 2:
-            rev_list_cmd = ["git", f"--git-dir={analysisInput.git_dir}", "rev-list", "--count", f"{parents[0]}...{parents[1]}"]
-            entangled_commit_count = int(subprocess.check_output(rev_list_cmd, text=True))
+        rev_list_cmd = ["git", f"--git-dir={analysisInput.git_dir}", "rev-list", "--count"] + parents
+        if merge_bases:
+            rev_list_cmd += ["--not"] + merge_bases
+        entangled_commit_count = int(subprocess.check_output(rev_list_cmd, text=True))
 
         return (
             analysisInput,
