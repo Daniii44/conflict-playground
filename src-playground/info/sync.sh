88d022ed10447807034934ee656f5c71756523fa
Daniel Gruber <daniel.gruber@altofing.de>
2026-05-27 11:05:33 +0200

    feat(info-sync): Require exactly one playbook name

--- src-playground/info/sync.sh
+++ src-playground/info/sync.sh
@@ -2,23 +2,12 @@
 
 set -euo pipefail
 
-if [ "$#" -gt 1 ]; then
+if [ "$#" -ne 1 ]; then
     echo "Usage: info-sync [playbook]" >&2
     exit 1
 fi
 
-if [ "$#" -eq 1 ]; then
-    repos=$(playbook-repos "$1")
-else
-    repos=$(
-        find "${CACHES}/repos" -type f -name HEAD | while IFS= read -r head; do
-            repo_dir=$(dirname "$head")
-            if [ -d "${repo_dir}/objects" ]; then
-                realpath -L --relative-to="${CACHES}/repos" "$repo_dir"
-            fi
-        done
-    )
-fi
+repos=$(playbook-repos "$1")
 
 for repo in $repos; do
     echo "Processing: $repo"
