57d9c8281e853ee2b268e9cdcaf4eddf896eeaa6
Daniel Gruber <daniel.gruber@altofing.de>
2026-05-30 00:10:57 +0200

    feat(analysis): Added submodule analysis
    Creates playground to replay merges with initialized submodules

--- src-playground/info/conflict/sync.py
+++ src-playground/info/conflict/sync.py
@@ -17,6 +17,7 @@ from common.redis_util import setup_redis_connection
 from info.conflict.analysis.common import Analysis, AnalysisInput
 from info.conflict.analysis.core_analysis import CoreAnalysis
 from info.conflict.analysis.divergence_analysis import DivergenceAnalysis
+from info.conflict.analysis.submodule import SubmoduleAnalysis
 from info.conflict.analysis.tree_diff_analysis import TreeDiffAnalysis
 
 # Script to collect all dirty merge commits in a bare git repository
@@ -34,6 +35,7 @@ exiting = False
 AVAILABLE_ANALYSES: dict[str, type[Analysis]] = {
     "core": CoreAnalysis,
     "divergence": DivergenceAnalysis,
+    "submodule": SubmoduleAnalysis,
     "tree-diff": TreeDiffAnalysis,
 }
 
